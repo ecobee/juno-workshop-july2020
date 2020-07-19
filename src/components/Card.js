36f2bbee7b64f947f33fe5b7f25cf6cdc429c132
Prasanth Pillai <prasanthp@ecobee.com>
2020-07-19 15:01:40 -0400

    cleanup

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,29 +2,6 @@ import React from "react";
 import Img from "./Img";
 import ReactModal from "react-modal";
 
-// function Card({ movie }) {
-//   return (
-//     <div className="card">
-//       <h3>{movie.title}</h3>
-//       <h4>({movie.release_date})</h4>
-//       <Img
-//         path={movie.poster_path}
-//         title={movie.title}
-//         vote={movie.vote_average}
-//         date={movie.release_date}
-//       />
-//       <span>
-//         <p>
-//           {movie.vote_average}
-//           <svg height="20" width="23" class="star rating" data-rating="1">
-//             <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" />
-//           </svg>
-//         </p>
-//       </span>
-//     </div>
-//   );
-// }
-
 class Card extends React.Component {
   constructor(props) {
     super(props);
