5d514e0c4007599f979978a1fb3bf6a8e56b1db5
Prasanth Pillai <prasanthp@ecobee.com>
2020-07-19 14:22:29 -0400

    adds release date / viter average

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Img from "./Img";
 
+
 function Card({ movie }) {
   return (
     <div className="card">
-      <Img path={movie.poster_path} title={movie.title} />
-      <p>{movie.title}</p>
+      <h3>{movie.title}</h3>
+      <h4>({movie.release_date})</h4>
+      <Img
+        path={movie.poster_path}
+        title={movie.title}
+        vote={movie.vote_average}
+        date={movie.release_date}
+      />
+      <span>
+        <p>
+          {movie.vote_average}
+          <svg height="20" width="23" class="star rating" data-rating="1">
+            <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" />
+          </svg>
+        </p>
+      </span>
     </div>
   );
 }
