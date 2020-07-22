6863ff8adc33e33dcbac34ab941fcc8ae2653690
Sylvan Mably <sylvan@gmail.com>
2020-07-21 22:40:05 -0400

    Remove unused boilerplate and fix console errors

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders main app title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = getByText('Juno x ecobee: Pair Programming Exercise');
+
+  expect(titleElement).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const rootElement = document.getElementById('root')
+
+Modal.setAppElement(rootElement)
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
